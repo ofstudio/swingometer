bfbb77fdad5f20d346f38448e6b6fbecc5f792d8
Oleg Fomin <ofstudio@gmail.com>
2019-12-10 19:07:09 +0300

    fix useEfffect warn

--- src/components/AuthCheck.js
+++ src/components/AuthCheck.js
@@ -8,15 +8,14 @@ const authCheck = process.env.REACT_APP_AUTH_CHECK !== 'false'
 
 export default function AuthCheck({isAuth, dispatch}) {
   let timeoutHandler = null
-  if (authCheck)
-    useEffect(
-      () => {
-        if (!isAuth)
-          timeoutHandler = window.setTimeout(
-            () => dispatch({type: 'lock', lockMessage: 'Неавторизованный доступ! Введите пин-код'}),
-            timings.lockTimeout)
-        return () => window.clearTimeout(timeoutHandler)
-      }, [])
+  useEffect(
+    () => {
+      if (authCheck && !isAuth)
+        timeoutHandler = window.setTimeout(
+          () => dispatch({type: 'lock', lockMessage: 'Неавторизованный доступ! Введите пин-код'}),
+          timings.lockTimeout)
+      return () => window.clearTimeout(timeoutHandler)
+    }, [])
 
   // noinspection JSConstructorReturnsPrimitive
   return null
